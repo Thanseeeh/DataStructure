0bb48d4de8d53cbbb8364648f8f9e1710caa2dae
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-06-09 20:22:41 +0530

    Bst and Graph complete code

--- DS-week3-Q15.py
+++ DS-week3-Q15.py
@@ -176,6 +176,8 @@ print("_________________________________________________________________________
 
 
 
+
+
 #BinaryHeap
 class MaxHeap:
     def __init__(self):
@@ -211,6 +213,17 @@ class MaxHeap:
             self.heapify(0)
             return max_val
         
+    def delete(self, x):
+        if self.heap is None:
+            print("Heap is empty")
+        else:
+            if x in self.heap:
+                index = self.heap.index(x)
+                parent = (index - 1) // 2
+                self.heap.pop(index)
+                self.heapify_up(parent)
+                self.heapify(0)
+        
     def heapify(self, i):
         left = (2*i) + 1
         right = (2*i) + 2
@@ -224,6 +237,22 @@ class MaxHeap:
         if largest != i:
             self.heap[largest], self.heap[i] = self.heap[i], self.heap[largest]
             self.heapify(largest)
+
+    def heap_sort(self):
+        sorted_heap = []
+        while len(self.heap) > 0:
+            max_val = self.extract_max()
+            sorted_heap.insert(0, max_val)
+        return sorted_heap
+    
+    def build_heap(self, array):
+        self.heap = array
+        n = len(array)
+        last_parent = (n//2) - 1
+
+        for i in range(last_parent, -1, -1):
+            self.heapify(i)
+        return self.heap
 
 heap = MaxHeap()
 heap.insert(12)
@@ -240,3 +269,117 @@ print("Max value        = ", maximum)
 print("Extracted max    = ", end=" ")
 heap.extract_max()
 print(heap.heap)
+print("Node deleted     = ", end=" ")
+heap.delete(12)
+print(heap.heap)
+print("Sorted Heap      = ", end=" ")
+sort = heap.heap_sort()
+print(sort)
+
+array = [4,3,1,7,4,6,1]
+build_heap = heap.build_heap(array)
+print("New Heap builded = ", end=" ")
+print(build_heap)
+print("_____________________________________________________________________________")
+
+
+
+
+
+#Graph
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, v):
+        if v in self.graph:
+            print("Node is already present")
+        else:
+            self.graph[v] = []
+    
+    def add_edge(self, v1, v2):
+        if v1 not in self.graph or v2 not in self.graph:
+            print("These elements not found in graph")
+        else:
+            self.graph[v1].append(v2)
+            self.graph[v2].append(v1)
+
+    def delete_node(self, v):
+        if v not in self.graph:
+            print("Element not found")
+        else:
+            self.graph.pop(v)
+            for i in self.graph:
+                if v in self.graph[i]:
+                    self.graph[i].remove(v)
+
+    def delete_edge(self, v1, v2):
+        if v1 not in self.graph or v2 not in self.graph:
+            print("These elements not found in graph")
+        else:
+            self.graph[v1].remove(v2)
+            self.graph[v2].remove(v1)
+
+    def bfs(self, start):
+        self.visited = []
+        queue = []
+        queue.append(start)
+        self.visited.append(start)
+
+        while queue:
+            v = queue.pop(0)
+            print(v, end=" ")
+            for neighbor in self.graph[v]:
+                if neighbor not in self.visited:
+                    queue.append(neighbor)
+                    self.visited.append(neighbor)
+
+    def bfs_all(self):
+        self.visited = []
+        for i in self.graph:
+            if i not in self.visited:
+                self.bfs(i)
+
+    def dfs(self, start):
+        self.visited = []
+        self._dfs(start)
+
+    def _dfs(self, v):
+        self.visited.append(v)
+        print(v, end=" ")
+        for i in self.graph[v]:
+            if i not in self.visited:
+                self._dfs(i)
+
+    def dfs_all(self):
+        self.visited = []
+        for i in self.graph:
+            if i not in self.visited:
+                self.dfs(i)
+
+graph = Graph()
+graph.add_node(1)
+graph.add_node(2)
+graph.add_node(3)
+graph.add_node(4)
+
+graph.add_edge(1,4)
+graph.add_edge(1,3)
+graph.add_edge(2,4)
+graph.add_edge(2,1)
+
+print()
+print("Graph        = ", end=" ")
+print(graph.graph)
+print("Node deleted = ", end=" ")
+graph.delete_node(2)
+print(graph.graph)
+print("Edge deleted = ", end=" ")
+graph.delete_edge(1,4)
+print(graph.graph)
+
+print("Bfs of graph = ", end=" ")
+graph.bfs_all()
+print()
+print("Dfs of graph = ", end=" ")
+graph.dfs_all()
